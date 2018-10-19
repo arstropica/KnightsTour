0791f83b302fdb43bbdf7278c77eb812d8989c05
admin <admin@192.168.50.148>
2018-10-18 21:30:26 -0400

    Echo starting position.

--- index.php
+++ index.php
@@ -5,17 +5,18 @@ use Tour\Knight;
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-$loc = [1,1];
 $size = 8;
-if (isset($argv[1], $argv[2])) {
-    $loc = [$argv[1], $argv[2]];
-}
 if (isset($argv[3])) {
     $size = $argv[3];
+}
+$loc = [rand(1, $size), rand(1, $size)];
+if (isset($argv[1], $argv[2])) {
+    $loc = [$argv[1], $argv[2]];
 }
 
 $board = new Board($loc, $size);
 $knight = new Knight($board);
+print "Starting Position: " . implode(", ", $loc) . "\n";
 try {
     $knight->explore(pow($size, 2) * 2);
     print $board->getCounter() . " / " . pow($size, 2) . " squares covered.\n";
