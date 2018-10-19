0829dc210cca2abc09bfc32678e5d8ff17955448
admin <admin@192.168.50.148>
2018-10-19 01:43:33 -0400

    Implementation of Warnsdorff's algorithm for finding closed tours
    - Loop through random starting positions until a closed tour is found
    - Report total move count for all tours
    - Report total number of tours

--- index.php
+++ index.php
@@ -9,21 +9,38 @@ $size = 8;
 if (isset($argv[3])) {
     $size = $argv[3];
 }
+
+// 1. Set P to be a random initial position on the board
 $loc = [rand(1, $size), rand(1, $size)];
 if (isset($argv[1], $argv[2])) {
     $loc = [$argv[1], $argv[2]];
 }
 
-$board = new Board($loc, $size);
-$knight = new Knight($board);
-print "Starting Position: " . implode(", ", $loc) . "\n";
-try {
-    $knight->explore(pow($size, 2) * 2);
-    print $board->getCounter() . " / " . pow($size, 2) . " squares covered.\n";
-    print "Total Number of Calculated Moves: " . $knight->getNumMoves() . ".\n";
-    
-    print_r($board->getMap(true));
-} catch (\Exception $e) {
-    print $e->getMessage() . "\n";
+$result = false;
+$counter = 0;
+$kmoves = 0;
+while (!$result) {
+    $counter ++;
+    $board = new Board($loc, $size);
+    $knight = new Knight($board);
+    print "New Starting Position: " . strtoupper(implode("", $board->numToAddress($loc))) . " [" . implode(",", $loc) . "].\n";
+    try {
+        $result = $knight->explore_warnsdorff(pow($size, 2) * 2);
+        $kmoves += $knight->getNumMoves();
+        if (! $result) {
+            // 1. Set P to be a random initial position on the board
+            $loc = [rand(1, $size), rand(1, $size)];
+        } else {
+            print $counter . " tour(s) performed.\n";
+            print $board->getCounter() . " / " . pow($size, 2) . " squares covered.\n";
+            print "Total Number of Calculated Moves: " . $kmoves . ".\n";
+            
+            // 4. Return the marked board — each square will be marked with the move number on which it is visited
+            print_r($board->getMap(true));
+        }
+    } catch (\Exception $e) {
+        print $e->getMessage() . "\n";
+        exit;
+    }
+    print "\n";
 }
-print "\n";
